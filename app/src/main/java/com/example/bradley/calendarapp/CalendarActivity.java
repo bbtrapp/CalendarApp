c75c7bf52f5908031fbc1e338be9296dff7fe56c
bbtrapp <bbtrapp@wisc.edu>
2016-03-08 11:25:17 -0600

    basic layouts set up. Transition from start to calendar set up. Calendar fragment started.

---
+++ app/src/main/java/com/example/bradley/calendarapp/CalendarActivity.java
@@ -0,0 +1,25 @@
+package com.example.bradley.calendarapp;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalendarActivity extends AppCompatActivity implements
+    CalendarFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_fragment_container, CalendarFragment.newInstance(null, null))
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
